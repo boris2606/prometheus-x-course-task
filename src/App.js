9863f3fa301a15e6b18ace849b444447d0d944a1
Boris Shagiev <boris260692@gmail.com>
2023-08-08 02:27:36 +0300

    Change App js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import './theme-styles/Theme.css'
-import books from './data/books.json'
+import Books from './data/books.json'
 import SignIn from "./pages/signin/SignIn"
 import Context from './data/Context';
 import BookList from './pages/book-list/BookList';
 import ProtectedRoutes from './ProtectedRoutes';
 import SpecificBook from './pages/specific-book/SpecificBook'
 import Cart from './pages/cart/Cart';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Error from './pages/error-page/Error';
 import Layout from './components/Layout';
 
@@ -19,8 +19,23 @@ function App() {
   const [theme,setTheme] = useState(false)
 
   const data = {
-    ...books
+    ...Books
   }
+
+  // Можна використати фетч, але лише якщо .json файл знаходиться в папці public
+  // const data = JSON.parse(localStorage.getItem('data'))
+
+  // const fetchData = async () => {
+  //   try {
+  //     await fetch('books.json')
+  //             .then(response => response.json())
+  //             .then(dataBooks => {
+  //               localStorage.setItem('data', JSON.stringify(dataBooks))
+  //             })
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 
   useEffect(()=> { 
     setCardBooks(JSON.parse(localStorage.getItem('cardBook')) || [])
